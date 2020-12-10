9b2c0ef40dcc1c7601ffc206c99ec99f17fc2022
Luis Correa Bruna <lu.correab@alumnos.duoc.cl>
2020-12-10 19:45:45 -0300

    feat: focus working properly

--- src/index.js
+++ src/index.js
@@ -1,46 +1,62 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const { app, BrowserWindow, Tray, Menu } = electron;
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+let tray = null;
+
+if (require('electron-squirrel-startup')) {
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+    // const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    const mainWindow = new BrowserWindow({
+        // width: width,
+        // height: height,
+        frame: false,
+        resizable: false,
+        fullscreen: true,
+        transparent: true,
+        webPreferences: {
+            enableRemoteModule: true,
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+        },
+    });
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.setIgnoreMouseEvents(true, { forward: true });
+
+    mainWindow.setAlwaysOnTop(true, 'screen-saver');
+    // mainWindow.
+    mainWindow.setVisibleOnAllWorkspaces(true);
+    // mainWindow.setFullScreenable(false);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+const createTray = () => {
+    tray = new Tray(path.join(__dirname, './img/1.jpg'));
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' },
+    ]);
+    tray.setToolTip('This is my application.');
+    tray.setContextMenu(contextMenu);
+};
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.on('ready', createWindow);
+app.on('ready', createTray);
+
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
---
+++ src/preload.js
@@ -0,0 +1,39 @@
+const remote = require('electron').remote;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type]);
+    }
+
+    let isUsingTool = false;
+
+    const mainWindow = remote.getCurrentWindow();
+
+    const toolBar = document.getElementById('toolBar');
+
+    toolBar.addEventListener('mouseleave', () => {
+        if (!isUsingTool) {
+            mainWindow.setIgnoreMouseEvents(true, { forward: true });
+        }
+    });
+    toolBar.addEventListener('mouseenter', () => {
+        mainWindow.setIgnoreMouseEvents(false);
+    });
+
+    const tools = document.querySelectorAll('.tool-btn');
+    tools.forEach((tool) => {
+        tool.addEventListener('click', () => {
+            if (!isUsingTool) {
+                mainWindow.setIgnoreMouseEvents(false);
+                isUsingTool = true;
+            } else {
+                isUsingTool = false;
+            }
+        });
+    });
+});
